11b65bb95a472917d1ebac02f4cbe7239c3f81ca
Isaac Greenwald <i_greenwald@coloradocollege.edu>
2024-07-20 21:42:29 -0600

    interactive changes w javascript

---
+++ js/35mm.js
@@ -0,0 +1,50 @@
+
+function enlargeImage(id){
+    
+    const image1 = document.getElementById(id);
+    const back1 = document.getElementById("background1");
+    const back2 = document.getElementById("background2");
+    const body1 = document.getElementById("body1");
+    let w = image1.clientWidth;
+    let h = image1.clientHeight;
+    let left = image1.clientLeft;
+    let top = image1.clientTop;
+
+    if (image1.classList.contains("small")){
+        let screenw = window.innerWidth;
+        let screenh = window.innerHeight;
+        image1.classList.remove("small");
+        image1.classList.add("big");
+        let doubleh = h*2;
+        let doublew = w*2;
+        image1.style.height = doubleh.toString() + 'px';
+        image1.style.width = doublew.toString() + 'px';
+        image1.style.zIndex = "2";
+        image1.style.position = "fixed";
+        image1.style.left = (screenw/2 - w).toString() + 'px';
+        image1.style.top = (screenh/2 - h).toString() + 'px';
+        back1.style.zIndex="1";
+        back2.style.zIndex="1";
+        body1.classList.add("scroll");
+
+    }
+    else {
+        image1.classList.remove("big");
+        image1.classList.add("small")
+        let halfh = h/2;
+        let halfw = w/2;
+        image1.style.height = halfh.toString() + 'px';
+        image1.style.width = halfw.toString() + 'px';
+        image1.style.zIndex = "0";
+        image1.style.left=left.toString() + 'px';
+        image1.style.top=top.toString() + 'px';
+        image1.style.position = "relative";
+        back1.style.zIndex="-1";
+        back2.style.zIndex="-1";
+        body1.classList.remove("scroll");
+
+    }
+
+
+}
+
