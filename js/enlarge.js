e13382a5cbf64921fb9d1377ab9e73ccfd16ed75
Isaac Greenwald <i_greenwald@coloradocollege.edu>
2024-09-08 06:40:07 -0600

    fix enlarging image bug

--- js/enlarge.js
+++ js/enlarge.js
@@ -22,9 +22,10 @@ function enlargeImage(id){
         image1.style.left = (screenw/2 - w).toString() + 'px';
         image1.style.top = (screenh/2 - h).toString() + 'px';
         back1.style.zIndex="1";
+        image1.style.zIndex="2";
         body1.classList.add("scroll");
     }
-    else {
+    else if (image1.classList.contains("big")) {
         image1.classList.remove("big");
         image1.classList.add("small")
         let halfh = h/2;
@@ -35,10 +36,10 @@ function enlargeImage(id){
         image1.style.left=left.toString() + 'px';
         image1.style.top=top.toString() + 'px';
         image1.style.position = "relative";
+        image1.style.zIndex="0";
         back1.style.zIndex="-1";
         body1.classList.remove("scroll");
     }
 
 
 }
-
