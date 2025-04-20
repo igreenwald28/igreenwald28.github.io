8d7fe2aa096613e3cc21b1d8ff8da19c4d4db416
Isaac Greenwald <i_greenwald@coloradocollege.edu>
2025-04-19 19:54:21 -0600

    expand film page

--- src/app/35mm/page.tsx
+++ src/app/35mm/page.tsx
@@ -6,33 +6,22 @@ import './Film.css';
 
 export default function Film() {
   const images = (imageData as ImageDataJSON).collections.film;
-  const rowHeight = 280; 
 
   return (
     <LocationLayout>
       <div className="film-container">
-        {images.map((image, index) => {
-          const isLandscape = image.orientation === 'landscape';
-          const aspectRatio = isLandscape ? 3/2 : 2/3;
-          const width = rowHeight * aspectRatio;
-
-          return (
-            <div
-              key={index}
-              className={`film-item ${image.orientation}`}
-              style={{
-                width: `${width}px`,
-                height: `${rowHeight}px`
-              }}
-            >
-              <img
-                src={image.src}
-                alt={`Film photo ${index + 1}`}
-                className="film-image"
-              />
-            </div>
-          );
-        })}
+        {images.map((image, index) => (
+          <div
+            key={index}
+            className={`film-item ${image.orientation}`}
+          >
+            <img
+              src={image.src}
+              alt={`Film photo ${index + 1}`}
+              className="film-image"
+            />
+          </div>
+        ))}
       </div>
     </LocationLayout>
   );
